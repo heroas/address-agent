aaab1b512c77b087aa19f82fb858b25b801670d1
Heroas <heroas@protagonist.xyz>
2018-01-31 21:02:48 -0500

    Fixing addressBook importer

--- src/services/utilities.js
+++ src/services/utilities.js
@@ -4,8 +4,8 @@ export default {
     for (var i = 0, len = addressBook.length; i < len; i++) {
       if (!addressBook[i].divider) {
 
-        if (addressBook[i].address && addressBook[i].avatar && addressBook[i].subtitle &&
-          addressBook[i].title && addressBook[i].worth)
+        if (addressBook[i].address && addressBook[i].avatar && addressBook[i].symbol &&
+          addressBook[i].name && addressBook[i].worth && addressBook[i].balance)
           return true;
         else
           return false;
